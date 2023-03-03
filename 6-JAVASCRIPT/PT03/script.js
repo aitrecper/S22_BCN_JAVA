ef227457113a633bb123075857c4feb2c905e971
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-03 15:55:38 +0100

    JS-PT03

---
+++ 6-JAVASCRIPT/PT03/script.js
@@ -0,0 +1,33 @@
+var $string;
+var $number = 123;
+var $boolean = true;
+var $null = null;
+var $undefined = undefined;
+var $simbol = Symbol('Algo');
+var $array = [1,"hola",false];
+var $object = {'llave' : 'Valor'};
+var $function = function saludo(){
+    alert('Soy una función');
+};
+
+$string = "Hola";
+
+var i = 'global';
+
+function hola(){
+    var i = 'local';
+    console.log(i);
+}
+console.log(i);
+hola();
+
+
+console.log(typeof $string + " " + $string);
+console.log(typeof $number + " " + $number);
+console.log(typeof $boolean + " " + $boolean);
+console.log(typeof $null + " " + $null);
+console.log(typeof $undefined + " " + $undefined);
+//console.log("Simbol " + $simbol);
+console.log(typeof $array + " " + $array);
+console.log(typeof $object + " " + $object);
+console.log(typeof $function + " " + $function);
