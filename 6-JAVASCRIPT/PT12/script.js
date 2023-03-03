36a3720d3b30c2472c147b213d2c09eae1ac4bf7
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-03 18:22:50 +0100

    JS-PT12

---
+++ 6-JAVASCRIPT/PT12/script.js
@@ -0,0 +1,10 @@
+let array = ['a','b','c','d','e'];
+
+array.forEach(element=>{
+    console.log(element);
+});
+console.log();
+let numero = 5;
+for(numero; numero >= 0; numero--){
+    console.log(array[numero]);
+}
