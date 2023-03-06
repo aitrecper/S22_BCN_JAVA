6d2aa02b6eb8cba510d7983f1205d09acd1dbb7c
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-06 18:09:22 +0100

    JS-PT18-bugfix

--- 6-JAVASCRIPT/PT18/script.js
+++ 6-JAVASCRIPT/PT18/script.js
@@ -1,9 +1,9 @@
 $().ready(() => {
     $('img').hover(
-        () => {
+        function () {
             $(this).attr("src", "./img/card-front.png");
         },
-        () => {
+        function () {
             $(this).attr("src", "./img/card-back.png");
         }
     );
