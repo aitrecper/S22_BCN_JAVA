2ac44c9f65ebe1a547e1f790438da61f15b40cea
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-14 18:51:22 +0100

    JAVA-PT9

---
+++ 8-JAVA/PT09/src/com/recio/aitor/pt09.java
@@ -0,0 +1,20 @@
+package com.recio.aitor;
+
+public class pt09 {
+	public static void main(String[] args) {
+		String name = "Aitor";
+		String surname = "Recio Pérez";
+		String street = "Calle";
+		String city = "Barcelona";
+		String zipCode = "08035";
+		String sex = "Hombre";
+		
+		System.out.println("Información personal:\n" +
+				"\tNombre: "+ name + "\n" +
+				"\tApellido: " + surname + "\n"+ 
+				"\tCalle: " + street + "\n" + 
+				"\tMunicipio: " + city + "\n" +
+				"\tCódigo Postal: " + zipCode + "\n" +
+				"\tSexo: "+sex);
+	}
+}
