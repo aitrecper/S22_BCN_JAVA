fa59eb615cc9d9d51b71ca86f2e2cf97cd4ff3da
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-14 18:07:46 +0100

    JAVA-PT7

---
+++ 8-JAVA/PT07/src/com/recio/aitor/Main.java
@@ -0,0 +1,37 @@
+package com.recio.aitor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Lógicos
+		final boolean val1 = true;
+		System.out.println(val1);
+		
+		//Carácter
+		final char val2 = 'a';
+		System.out.println(val2);
+		
+		//Numéricos enteros
+		final byte val3 = 1;
+		final short val4 = 2;
+		final int val5 = 3;
+		final long val6 = 4;
+		System.out.println(val3);
+		System.out.println(val4);
+		System.out.println(val5);
+		System.out.println(val6);
+		
+		//Numéricos reales
+		final float val7 = 6.00f;
+		final double val8 = 7.00;
+		System.out.println(val7);
+		System.out.println(val8);
+		
+		//Constantes
+		final String NOMBRE = "Aitor";
+		System.out.println(NOMBRE);
+	}
+
+}
