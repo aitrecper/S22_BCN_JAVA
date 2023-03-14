a8fcdac3aa6767a857c06159ddf32f6c8626582d
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-14 17:03:50 +0100

    JAVA-PT3

---
+++ 8-JAVA/PT3/src/com/recio/aitor/pt3.java
@@ -0,0 +1,10 @@
+package com.recio.aitor;
+
+public class pt3 {
+	public static void main(String[] args) {
+		System.out.println("############Menú############");
+		System.out.println("\t1.-Opción 1\n\t2.-Opción 2");
+		System.out.println("\\o/ (\'persona\' levantando los \"brazos\")");
+		System.out.println(" |\n/\\");
+	}
+}
