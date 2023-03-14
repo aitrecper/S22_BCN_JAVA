6b20b217149a63034df507be3a5faa33d86b3a5c
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-14 19:03:27 +0100

    JAVA-PT10

---
+++ 8-JAVA/PT10/src/com/recio/aitor/pt10.java
@@ -0,0 +1,13 @@
+package com.recio.aitor;
+
+public class pt10 {
+	public static void main(String[] args) {
+		System.out.println(5-10);
+		System.out.println(55+45);
+		System.out.println(-3);
+		System.out.println(5*5);
+		System.out.println(25/7);
+		System.out.println(25/7.00);
+		System.out.println(25%4);
+	}
+}
