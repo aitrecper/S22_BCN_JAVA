8a400704ddf356517b80c73d67770980612f2c80
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-14 19:49:10 +0100

    JAVA-PT14

---
+++ 8-JAVA/PT14/src/com/recio/aitor/pt14.java
@@ -0,0 +1,19 @@
+package com.recio.aitor;
+
+public class pt14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean bool1 = true;
+		boolean bool2 = false;
+		boolean bool3 = false;
+		
+		if(bool1 == !bool2)
+			System.out.println("bool1 es igual al valor invertido de bool2");
+		
+		if(bool2 == !bool3)
+			System.out.println("bool2 es igual al vaor invertido de bool3");
+	}
+
+}
