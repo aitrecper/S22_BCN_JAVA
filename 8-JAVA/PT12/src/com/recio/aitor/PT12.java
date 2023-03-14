86837a8bd47da818f721d7a2cf860218b47fb73e
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-14 19:26:43 +0100

    JAVA-PT12

---
+++ 8-JAVA/PT12/src/com/recio/aitor/PT12.java
@@ -0,0 +1,25 @@
+package com.recio.aitor;
+
+public class PT12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 25;
+		num+=5;
+		System.out.println("Suma: " + num);
+		
+		num-=15;
+		System.out.println("Resta: " + num);
+		
+		num*=2;
+		System.out.println("Multiplicación: " + num);
+		
+		num/=2;
+		System.out.println("División: " + num);
+		
+		num%=3;
+		System.out.println("Cociente: " + num);
+	}
+
+}
