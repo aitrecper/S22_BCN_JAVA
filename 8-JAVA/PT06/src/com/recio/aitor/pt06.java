9d2ae80d57728798bfc46a3a2e96d93d44e6e81f
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-14 17:30:08 +0100

    JAVA-PT6

---
+++ 8-JAVA/PT06/src/com/recio/aitor/pt06.java
@@ -0,0 +1,19 @@
+package com.recio.aitor;
+
+public class pt06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte val1 = (byte) (Byte.MAX_VALUE+5);
+		short val2 = (short) (Short.MAX_VALUE+25);
+		int val3 = Integer.MAX_VALUE+10000;
+		long val4 = Long.MAX_VALUE+20000;
+		
+		System.out.println("val1: "+val1);
+		System.out.println("val2: "+val2);
+		System.out.println("val3: "+val3);
+		System.out.println("val4: "+val4);
+	}
+
+}
