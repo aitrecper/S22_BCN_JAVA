9a250784b220e094270102b8623316a54a1d4b72
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-14 17:12:44 +0100

    JAVA-PT4

---
+++ 8-JAVA/PT04/src/com/recio/aitor/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.recio.aitor;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = 	10; // Creamos la variable num1
+		int num2 = 20; // Creamos la variable num2
+		int suma = num1 + num2; // Creamos la variable suma que es la suma entre num1 y num2
+		System.out.println(suma); // Mostramos el resultado por consola de suma
+	}
+
+}
---
+++ 8-JAVA/PT2/ILoveJava.java
@@ -0,0 +1,6 @@
+
+public class ILoveJava {
+	public static void main(String[] args) {
+		System.out.println("I♥Java");
+	}
+}
