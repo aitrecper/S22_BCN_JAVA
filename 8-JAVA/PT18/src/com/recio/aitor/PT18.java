072aa166a19791550dc506e16f91b531c2a09324
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-16 16:21:22 +0100

    JAVA-PT18

---
+++ 8-JAVA/PT18/src/com/recio/aitor/PT18.java
@@ -0,0 +1,44 @@
+package com.recio.aitor;
+
+public class PT18 {
+	public static void main(String[] args) {
+		int altura = 10;
+        int espacios = altura - 1;
+        int asteriscos = 2;
+
+        // Dibujar la parte superior del árbol
+        for (int i = 0; i < altura; i++) {
+            // Dibujar los espacios en cada nivel
+            for (int j = 0; j < espacios; j++) {
+                System.out.print(" ");
+            }
+            // Dibujar los asteriscos en cada nivel
+            for (int j = 0; j < asteriscos; j++) {
+                System.out.print("*");
+            }
+            // Cambiar de línea después de cada nivel
+            System.out.println();
+            // Actualizar el número de espacios y asteriscos para el siguiente nivel
+            espacios--;
+            asteriscos += 2;
+        }
+
+        // Dibujar el tronco del árbol
+        int alturaTronco = 2;
+        int anchoTronco = 4;
+        espacios = altura - 2;
+
+        for (int i = 0; i < alturaTronco; i++) {
+            // Dibujar los espacios del tronco
+            for (int j = 0; j < espacios; j++) {
+                System.out.print(" ");
+            }
+            // Dibujar los asteriscos del tronco
+            for (int j = 0; j < anchoTronco; j++) {
+                System.out.print("*");
+            }
+            // Cambiar de línea después de cada nivel
+            System.out.println();
+        }
+	}
+}
