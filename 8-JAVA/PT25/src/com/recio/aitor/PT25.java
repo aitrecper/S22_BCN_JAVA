8995f97e57cd22f2e7b533494b23146ecc889e58
Aitor Recio Pérez <aitrecper@gmail.com>
2023-03-17 19:10:01 +0100

    JAVA-PT25

---
+++ 8-JAVA/PT25/src/com/recio/aitor/PT25.java
@@ -0,0 +1,19 @@
+package com.recio.aitor;
+
+public class PT25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.printf("El tipo de datos: byte%nEstá formado por: %d Bytes%nUn byte está formado por %d Bits%nSu rango va del: %d a %d%n%n",Byte.BYTES,Byte.BYTES*8,Byte.MIN_VALUE,Byte.MAX_VALUE);
+		System.out.printf("El tipo de datos: short%nEstá formado por: %d Bytes%nUn short está formado por %d Bits%nSu rango va del: %d a %d%n%n",Short.BYTES,Short.BYTES*8,Short.MIN_VALUE,Short.MAX_VALUE);
+		System.out.printf("El tipo de datos: int%nEstá formado por: %d Bytes%nUn int está formado por %d Bits%nSu rango va del: %d a %d%n%n",Integer.BYTES,Integer.BYTES*8,Integer.MIN_VALUE,Integer.MAX_VALUE);
+		System.out.printf("El tipo de datos: long%nEstá formado por: %d Bytes%nUn long está formado por %d Bits%nSu rango va del: %s a %s%n%n",Long.BYTES,Long.BYTES*8,Long.MIN_VALUE,Long.MAX_VALUE);
+		System.out.printf("El tipo de datos: double%nEstá formado por: %d Bytes%nUn double está formado por %d Bits%nSu rango va del: %s a %s%n%n",Double.BYTES,Double.BYTES*8,Double.MIN_VALUE,Double.MAX_VALUE);
+		
+	
+	
+	
+	}
+
+}
