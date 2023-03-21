d1eb3970f7209a0da1797a075bcfa52a562b8af1
Aitor Recio Pérez <32538053+aitrecper@users.noreply.github.com>
2023-03-21 15:34:52 +0100

    Update PT26.java

--- 8-JAVA/PT26/src/com/recio/aitor/PT26.java
+++ 8-JAVA/PT26/src/com/recio/aitor/PT26.java
@@ -4,19 +4,41 @@ public class PT26 {
 
 	public static void main(String[] args) {
 		
-		//Declara una variable de tipo Long, byte, short e Integer
-		Long var1 = 1l;
-		byte var2 = 1;
-		short var3 = 1;
-		Integer var4 = 1;
+		Long numLong = 123456789L;
+		byte numByte = 127;
+		short numShort = 32767;
+		Integer numInteger = 2147483647;
 		
-		//¿Puedes obtener la clase a la que pertenecen todas ellas?
-		//    No
-		
-		//Ante el caso de que no pudieras, explica algunas maneras de resolver esto de las siguientes maneras:
-		//    Castear para identificar a Wrapper debería pertenecer
-		
+		Class claseLong = numLong.getClass();
+		Class claseByte = ((Byte) numByte).getClass();
+		Class claseShort = ((Short) numShort).getClass();
+		Class claseInteger = numInteger.getClass();
 		
 	}
 	
+	public static void identificarWrapper(Object objeto) {
+        if (objeto instanceof Boolean) {
+            System.out.println("El objeto es de tipo boolean y su wrapper correspondiente es Boolean");
+        } else if (objeto instanceof Character) {
+            System.out.println("El objeto es de tipo char y su wrapper correspondiente es Character");
+        } else if (objeto instanceof Byte) {
+            System.out.println("El objeto es de tipo byte y su wrapper correspondiente es Byte");
+        } else if (objeto instanceof Short) {
+            System.out.println("El objeto es de tipo short y su wrapper correspondiente es Short");
+        } else if (objeto instanceof Integer) {
+            System.out.println("El objeto es de tipo int y su wrapper correspondiente es Integer");
+        } else if (objeto instanceof Long) {
+            System.out.println("El objeto es de tipo long y su wrapper correspondiente es Long");
+        } else if (objeto instanceof Float) {
+            System.out.println("El objeto es de tipo float y su wrapper correspondiente es Float");
+        } else if (objeto instanceof Double) {
+            System.out.println("El objeto es de tipo double y su wrapper correspondiente es Double");
+        } else {
+            // Si el objeto no es ninguno de los tipos anteriores, se asume que es un tipo primitivo y se obtiene el wrapper correspondiente al castear a Object
+            String tipoPrimitivo = objeto.getClass().getSimpleName();
+            String nombreWrapper = Character.toUpperCase(tipoPrimitivo.charAt(0)) + tipoPrimitivo.substring(1);
+            System.out.println("El objeto es de tipo " + tipoPrimitivo + " y su wrapper correspondiente es " + nombreWrapper);
+        }
+    }
 }
+
